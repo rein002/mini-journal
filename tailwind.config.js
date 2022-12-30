bae6194e6ae45ed31f5fc50524d043f42eefacac
Kanta Shanai <zeromindful@gmail.com>
2022-12-30 13:07:45 +0900

    フォントの設定を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,37 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+        sans: [
+          'ヒラギノ角ゴシック',
+          '游ゴシック',
+          'メイリオ',
+          '游明朝',
+          'リュウミン',
+          'GEORGIA',
+          'Helvetica',
+          'Arial',
+          '"Noto Sans"',
+          '"Noto Serif"',
+          '"M Plus 1"',
+          '"Roboto"',
+          'Oswald',
+          'Montserrat',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms')],
 };
