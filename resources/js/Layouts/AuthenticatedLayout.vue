e62a30a63680d7d7a41a7f41695fa3a8ca02a22e
Kanta Shanai <zeromindful@gmail.com>
2022-12-30 11:41:39 +0900

    ziggy-jsのインポートと@ts-ignoreで解決できる型エラーを修正

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -123,9 +123,17 @@ const showingNavigationDropdown = ref(false);
           <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
               <div class="font-medium text-base text-gray-800">
-                {{ $page.props.auth.user.name }}
+                {{
+                  // @ts-ignore
+                  $page.props.auth.user.name
+                }}
               </div>
-              <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+              <div class="font-medium text-sm text-gray-500">
+                {{
+                  // @ts-ignore
+                  $page.props.auth.user.email
+                }}
+              </div>
             </div>
 
             <div class="mt-3 space-y-1">
