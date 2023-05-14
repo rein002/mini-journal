1c8706a3e424d1a9743daf1b3de87b767bbecc29
Kanta Shanai <zeromindful@gmail.com>
2023-05-14 16:01:49 +0900

    TypeScript化の準備

--- src/mini-journal/resources/js/app.js
+++ src/mini-journal/resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Ziggy } from './ziggy';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,10 +13,10 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
+        createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .use(ZiggyVue, Ziggy)
+        .mount(el);
     },
     progress: {
         color: '#4B5563',
