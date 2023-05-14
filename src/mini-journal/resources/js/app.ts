08901dd4c920581a9fe26c2bb243a35edd2b4e65
Kanta Shanai <zeromindful@gmail.com>
2023-05-14 21:11:42 +0900

    Vite環境をTypeScript化

--- src/mini-journal/resources/js/app.ts
+++ src/mini-journal/resources/js/app.ts
@@ -11,7 +11,7 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue') as any),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
         .use(plugin)
--- src/mini-journal/vite.config.ts
+++ src/mini-journal/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+// @ts-ignore
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: 'resources/js/app.ts',
             refresh: true,
         }),
         vue({
