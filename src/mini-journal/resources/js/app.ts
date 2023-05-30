90372acd2d2d20735bac8e15597116964317b72c
Kanta Shanai <zeromindful@gmail.com>
2023-05-30 15:46:19 +0900

    laravel-vue-i18nをインストールし、他言語化に対応

--- src/mini-journal/resources/js/app.ts
+++ src/mini-journal/resources/js/app.ts
@@ -6,17 +6,25 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Ziggy } from './ziggy';
+import { i18nVue } from 'laravel-vue-i18n';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue') as any),
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue') as any),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
-        .use(plugin)
-        .use(ZiggyVue, Ziggy)
-        .mount(el);
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .use(i18nVue, {
+                resolve: async (lang: string) => {
+                    const langs = import.meta.glob('../../lang/*.json');
+                    return await langs[`../../lang/${lang}.json`]();
+                },
+            })
+            .mount(el);
     },
     progress: {
         color: '#4B5563',
--- src/mini-journal/vite.config.ts
+++ src/mini-journal/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 // @ts-ignore
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import i18n from 'laravel-vue-i18n/vite';
 
 export default defineConfig({
     plugins: [
@@ -17,11 +18,12 @@ export default defineConfig({
                 },
             },
         }),
+        i18n(),
     ],
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: 'localhost'
-        }
-    }
+            host: 'localhost',
+        },
+    },
 });
